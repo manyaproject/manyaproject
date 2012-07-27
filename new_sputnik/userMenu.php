f16481c9f722d5b8116015b95645152676c68b63
Oleh Portyanin <ollieportnin>
2012-07-27 21:32:48 +0200

    released #9 as been requested

--- new_sputnik/userMenu.php
+++ new_sputnik/userMenu.php
@@ -1,23 +1,20 @@
 <?php
 session_start();
-if($_SERVER["REQUEST_METHOD"] == "post")
+if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	$user = $_SESSION["user"];
 	$pass = md5(trim(strip_tags($_POST['newPass'])));
-	echo $_SESSION["user"];
 	$newPass = md5(trim(strip_tags($_POST['newPassRepeat'])));
-	if($pass !== $newPass)
-	{
-		echo "Ви ввели не правильний пароль!!!";
-	}
 	require_once 'config.inc';
 	mysql_connect(DB_HOST, DB_USER, DB_PASS);
 	mysql_select_db(DB_NAME);
 	mysql_query("SET NAMES cp1251");
-	mysql_query("update subscriber set password=$pass where user='$user'") or die(mysql_error());
+	mysql_query("update subscriber set password='$pass' where user='$user'") or die(mysql_error());
 	mysql_close();
+    header("Location: logged_in.php");
 }
-?>
+?>   
+
 <html>
 <head>
 <?php
@@ -38,7 +35,7 @@ if($_SESSION['user'] === "Administrator")
 <script src="js/jseffects.js" type="text/javascript"> </script>
 </head>
 <body>
-<form action="<?=$_SERVER['PHP_SELF']?>" id="changePass" method="post">
+<form action="userMenu.php" id="changePass" method="post">
 Введіть новий пароль<input type="text" id="newPass" name="newPass"/>
 <p style="display:none" id="wrongPass">Ви ввели не правильний пароль. Пароль повинен містити лише латинські букви та цифри та знак:"_"</p>
 Повторіть новий пароль <input type="text" id="newPassRepeat" name="newPassRepeat"/>
